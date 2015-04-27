e643cb43f333570919c47eebd2a92293f9d7ea21
marianaviro <marianaviro@hotmail.com>
2015-04-26 19:10:00 -0500

    Agregué tema de Bootstrap con js y css

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,4 +1,14 @@
 //= require angular
 //= require angular-ui-router
+//= require bootstrap/dist/js/bootstrap.js
+//= require javascripts/jquery.prettyPhoto.js
+//= require javascripts/jquery.isotope.min.js
+//= require javascripts/main.js
+//= require javascripts/wow.min.js
+//= require javascripts/html5shiv.js
+//= require javascripts/respond.min.js
+
 //= require_tree .
+
+
 

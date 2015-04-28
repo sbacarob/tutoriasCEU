8ed6371101ed53b2883f1b678f315a12c161d957
ss1993 <s.sierra470@uniandes.edu.co>
2015-04-27 23:06:05 -0500

    "Pequeño error de registro de nombre de usuario corregido"

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,6 +13,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) << :major
+    devise_parameter_sanitizer.for(:sign_up) << :name
   end
 
 end
